9070b8566cd446fdae907156033b11b1fe112628
Anne Culpepper <anne.culpepper@gmail.com>
2020-03-15 10:15:56 -0500

    newEmployee data now pushed into employee array when button clicked. Info appears on the console

--- salaryCalc.js
+++ salaryCalc.js
@@ -1,5 +1,31 @@
 $(document).ready(init);
 
+const employees = [];
+
+//create the events to listen for
 function init() {
   console.log("Welcome to the salaryCalc!");
+  $(".js-inputEmployeeData").on("submit", submitEmployeeData);
+  //   $(".js-inputEmployeeData").on("click", clickEmployeeData);
 }
+
+function submitEmployeeData(event) {
+  console.log("Submit: " + event);
+  event.preventDefault();
+  console.log("This does not reload!");
+  const newEmployee = {
+    firstName: $(".js-firstNameInput").val(),
+    lastName: $(".js-lastNameInput").val(),
+    ID: parseFloat($(".js-idInput").val()),
+    Title: $(".js-titleInput").val(),
+    AnnualSalary: parseFloat($(".js-annualSalaryInput").val())
+  };
+  employees.push(newEmployee);
+  console.log("Employees in array: ", employees);
+}
+
+//function render() {}
+
+// function clickEmployeeData() {
+//   console.log("You clicked?");
+// }
